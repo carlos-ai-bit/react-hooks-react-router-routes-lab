783eaca7615fe5881dc69275a275e3cb6abd6729
Hoperaisers <abdallahsafari@gmail.com>
2023-06-13 12:50:54 +0300

    initial commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "flex",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  // background: "blue",
+  textDecoration: "none",
+  color: "blue",
+  listStyleType :"decimal"
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to='/'white
+        style={linkStyles}
+      >Home
+      </NavLink>
+      <NavLink to='/movies' style={linkStyles}>Movies</NavLink>
+      <NavLink to='/directors' style={linkStyles}>Directors</NavLink>
+      <NavLink to='/actors' style={linkStyles}>Actors</NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
